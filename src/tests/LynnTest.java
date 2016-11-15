c66e101ca1ab27b99a1e3de84e2f649e0b696123
lynnyou7 <1020361Teenie!>
2016-11-14 20:44:40 -0500

    Lynn JUnit Test updated

--- src/tests/LynnTest.java
+++ src/tests/LynnTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by lynnyou on 11/14/16.
@@ -21,14 +21,16 @@ public class LynnTest {
         profile = new Profile("username", "password", AccountType.MANAGER);
     }
     @Test(timeout = TIMEOUT)
-    public void testAddWaterSourceReport() {
+    public void testSearchProfile() {
         database.addProfile(profile);
-        assertTrue("profile was found", database.getProfiles()
-                .contains(profile));
+        assertEquals("profile was not found", profile, database.searchForProfile
+                ("username", "password"));
     }
 
-
-
-
+    @Test(timeout = TIMEOUT)
+    public void testSearchProfileNull() {
+        assertEquals("profile with that username and password was found", null,
+        database.searchForProfile("username", "password"));
+    }
 
 }
